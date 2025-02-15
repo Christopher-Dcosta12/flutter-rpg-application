b05206d688deeddaffdcb4f0acd3b1398a5e3a20
Christopher Dcosta <heron122000@gmail.com>
2025-02-15 12:33:12 +0530

    create custom theme for the application and add google font

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rpg/screens/home/home.dart';
+import 'package:flutter_rpg/theme.dart';
 
 void main() {
   runApp(MaterialApp(
+    theme: primaryTheme,
     home: Home(),
     // home: Sandbox(),
   ));
