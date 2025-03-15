e080c817531c07ca863efe40624efa2d64d4dcf9
Christopher Dcosta <heron122000@gmail.com>
2025-03-15 14:02:28 +0530

    develop player create screen and add input decoration theme

--- lib/shared/styled_text.dart
+++ lib/shared/styled_text.dart
@@ -2,15 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class StyledText extends StatelessWidget {
-  const StyledText(this.text, {super.key});
+  const StyledText(this.text, {super.key, this.centerText = false});
 
   final String text;
+  final bool centerText;
 
   @override
   Widget build(BuildContext context) {
-    return Text(text,
-        style: GoogleFonts.kanit(
-            textStyle: Theme.of(context).textTheme.bodyMedium));
+    return Text(
+      text,
+      style:
+          GoogleFonts.kanit(textStyle: Theme.of(context).textTheme.bodyMedium),
+      textAlign: centerText ? TextAlign.center : TextAlign.left,
+    );
   }
 }
 
